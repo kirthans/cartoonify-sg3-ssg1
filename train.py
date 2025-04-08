f4489a62d4e50d1869f1e37184fbc9bb895f8d3b
kirthans <kirthan.sg@gmail.com>
2025-04-08 12:58:45 +0530

    Fixed an error in the path

--- train.py
+++ train.py
@@ -33,9 +33,9 @@ class Pix2PixTrain:
         self.writer = SummaryWriter(self.log_dir)
 
     def setup_directories(self):
-        self.data_dir = Path(r"kaggle/working/processed_data")
-        self.log_dir = Path(r"kaggle/working/logs")
-        self.ckpt_dir = Path(r"kaggle/working/checkpoints")
+        self.data_dir = Path(r"/kaggle/working/processed_data")
+        self.log_dir = Path(r"/kaggle/working/logs")
+        self.ckpt_dir = Path(r"/kaggle/working/checkpoints")
         self.ckpt_dir.mkdir(exist_ok=True)
 
     def init_models(self):
